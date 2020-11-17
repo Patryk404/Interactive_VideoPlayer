cff6eb2c829b4ee63b321320018c968452adde27
Patrykosik88 <patryk744@op.pl>
2020-11-17 20:56:26 +0100

    Added Colors

--- GUI.py
+++ GUI.py
@@ -4,18 +4,28 @@ from player import Player
 class Gui:
     def __init__(self,title,path_video):
         self.window = Tk()
+        self.window.geometry("1500x800")
         self.window.title(title)
         self.player = Player(path_video,self.window,600,800)
         self.button_frame = Frame(self.window)
         self.button_frame.configure(bg="grey")
         self.button_frame.grid(row=1, column=0)
         self.scale_widget = 0
+        self.choose_color_list_box= 0
 
     def create_Interface(self):
         self.scale_widget=Scale(self.window,from_=0, to=self.player.duration, resolution=0.1, length=800, orient=HORIZONTAL)
         self.scale_widget.grid(row=2,column=0) 
+        self.choose_color_list_box = Listbox(self.window, width=20, height = 4)
+        self.choose_color_list_box.insert(END,"RED")
+        self.choose_color_list_box.insert(END,"GREEN")
+        self.choose_color_list_box.insert(END,"BLACK")
+        self.choose_color_list_box.insert(END,"PURPLE")
+        self.choose_color_list_box.select_set(1)
+        self.choose_color_list_box.place(x=1300,y=20)
         self.scale_widget.bind('<ButtonPress>',self.handle_Scale_click)   
-        self.scale_widget.bind('<ButtonRelease>',self.handle_Scale_release)                
+        self.scale_widget.bind('<ButtonRelease>',self.handle_Scale_release)      
+        self.choose_color_list_box.bind('<<ListboxSelect>>',self.handle_listbox_click)
         Button(self.button_frame,text="Play/Stop",width=10,bg="black", fg="white", command=self.handle_playButton_stopButton).grid(row=2, column=2)
         Button(self.button_frame,text="<<", width=5,bg="black", fg="white",command=self.handle_previousframeButton).grid(row=2,column =1)
         Button(self.button_frame,text=">>", width=5,bg="black", fg="white",command=self.handle_nextframeButton).grid(row=2,column =3)
@@ -25,8 +35,23 @@ class Gui:
     def handle_playButton_stopButton(self):
         self.player.play = not self.player.play
 
+    def handle_listbox_click(self,event):
+        cs = event.widget.curselection()
+
+        for  list in cs:
+            if list == 0:
+                self.player.draw.set_color("RED")
+            elif list == 1:
+                self.player.draw.set_color("GREEN")
+            elif list == 2:
+                self.player.draw.set_color("BLACK")
+            elif list == 3:
+                self.player.draw.set_color("PURPLE")
+
+
     def handle_resetButton(self):
-        self.player.frame = self.player.cap.read()[1]
+        self.player.nextFrame()
+        self.player.play = False
         self.player.draw.clear_screen()
 
     def handle_nextframeButton(self):
--- draw.py
+++ draw.py
@@ -1,8 +1,15 @@
 import cv2
+
+RED = (0,0,255)
+GREEN = (0,255,0)
+BLACK = (0,0,0)
+PURPLE = (128, 0, 128) 
+
 class Draw:
     def __init__(self,video):
         self.history = [[]]
         self.line_i = 0
+        self.color= GREEN
         video.bind("<B1-Motion>",self.draw)
         video.bind("<ButtonRelease-1>",self.handle_drawing)
 
@@ -17,10 +24,20 @@ class Draw:
     def draw(self,event):
         self.history[self.line_i].append((event.x,event.y))
 
+    def set_color(self,color):
+        if color=="RED":
+            self.color = RED
+        elif color =="GREEN":
+            self.color = GREEN
+        elif color =="BLACK":
+            self.color = BLACK
+        elif color =="PURPLE":
+            self.color = PURPLE
+
     def draw_lines(self,frame):
         for i in enumerate(self.history):
             for j in enumerate(i[1]):
                 try:
-                    cv2.line(frame,self.history[i[0]][j[0]],self.history[i[0]][j[0]+1],(0,255,0),thickness=2)
+                    cv2.line(frame,self.history[i[0]][j[0]],self.history[i[0]][j[0]+1],self.color,thickness=2)
                 except:
                     pass
